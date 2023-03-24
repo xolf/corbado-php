fb23e9df7c823c72811475217f896c07041d10c5
xolf <xolf@users.noreply.github.com>
2023-03-24 16:40:52 +0000

    Added verify_webhook()

--- src/CorbadoClient.php
+++ src/CorbadoClient.php
@@ -5,12 +5,15 @@ namespace Corbado;
 class CorbadoClient
 {
 
-    public function __construct(protected string $api_secret)
+    public function __construct(protected string $api_secret, protected ?string $webhook_user, protected ?string $webhook_password)
     {
 
     }
 
+    public function verify_webhook(): bool
+    {
+        return ($_SERVER['PHP_AUTH_USER'] ?? null) === $this->webhook_user && ($_SERVER['PHP_AUTH_PW'] ?? null) ===  $this->webhook_password;
+    }
 
-    
 
 }
