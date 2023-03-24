0efc21ad0a85095e011b4d2031e3a5ccf4941867
xolf <xolf@users.noreply.github.com>
2023-03-24 16:53:33 +0000

    Added handling for webhooks

--- src/CorbadoClient.php
+++ src/CorbadoClient.php
@@ -2,6 +2,8 @@
 
 namespace Corbado;
 
+use GuzzleHttp\Psr7\Response;
+
 class CorbadoClient
 {
 
@@ -17,6 +19,29 @@ class CorbadoClient
         }
 
         return ($_SERVER['PHP_AUTH_USER'] ?? null) === $this->webhook_user && ($_SERVER['PHP_AUTH_PW'] ?? null) ===  $this->webhook_password;
+    }
+
+    public function handle_webhook(?\Psr\Http\Message\RequestInterface $request, bool $exit = true)
+    {
+        $verified = $this->verify_webhook($request);
+
+        if ($verified) {
+            
+        } else {
+            if ($request) {
+                return new Response(401, [
+                    'WWW-Authentication' => 'Basic realm="Webhook"'
+                ], 'Unauthorized');
+            }
+    
+            header('WWW-Authenticate: Basic realm="Webhook"');
+            header('HTTP/1.0 401 Unauthorized');
+            echo 'Unauthorized.';
+    
+            if ($exit) {
+                exit(0);
+            }
+        }
     }
 
 
